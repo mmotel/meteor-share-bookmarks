74aea72cf19eb3627882888edd5661f9c794c65b
mmotel <mateuszmotel@gmail.com>
2015-04-13 13:39:33 +0200

    register validation

--- client/client.js
+++ client/client.js
@@ -5,3 +5,18 @@ UI.registerHelper('equals',
     return a === b;
   }
 );
+
+Valid = {
+  'username': function (name) {
+    var regexp = /^[a-z0-9_-]{3,16}$/;
+    return regexp.test(name);
+  },
+  'email': function (mail) {
+    var regexp = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    return regexp.test(mail);
+  },
+  'password': function (passwd) {
+    var regexp = /^[a-zA-Z0-9_-]{6,18}$/;
+    return regexp.test(passwd);
+  }
+}
--- client/helpers/auth.js
+++ client/helpers/auth.js
@@ -14,28 +14,60 @@ Template.header.events({
 });
 // register
 Template.register.events({
+  'keyup #register-username, blur #register-username': function (event, template) {
+    $('#register-error-username-used').addClass('hide');
+    var username = template.find('#register-username').value;
+    if(!Valid.username(username)){
+      $('#register-error-username-valid').removeClass('hide');
+    }
+    else {
+      $('#register-error-username-valid').addClass('hide');
+    }
+  },
+  'keyup #register-email, blur #register-email': function (event, template) {
+    var email = template.find('#register-email').value;
+    if(!Valid.email(email)){
+      $('#register-error-email').removeClass('hide');
+    }
+    else {
+      $('#register-error-email').addClass('hide');
+    }
+  },
+  'keyup #register-password, blur #register-password': function (event, template) {
+    var password = template.find('#register-password').value;
+    if(!Valid.password(password)){
+      $('#register-error-password').removeClass('hide');
+    }
+    else {
+      $('#register-error-password').addClass('hide');
+    }
+  },
   'submit form, click .submit': function (event, template) {
     event.preventDefault();
     var username = template.find('#register-username').value;
     var email = template.find('#register-email').value;
     var password = template.find('#register-password').value;
 
-    Accounts.createUser({
-        'username': username,
-        'email': email,
-        'password': password,
-        'profile': {
-          'name': username
-        }
-      },
-      function (error) {
-        if(error){
-          console.log(error);
-        }
-        else {
-          Router.go('/');
-        }
-      });
+    if(Valid.username(username) && Valid.email(email) &&
+     Valid.password(password)){
+      Accounts.createUser({
+          'username': username,
+          'email': email,
+          'password': password,
+          'profile': {
+            'name': username
+          }
+        },
+        function (error) {
+          if(error){
+            console.log(error);
+            $('#register-error-username-used').removeClass('hide');
+          }
+          else {
+            Router.go('/');
+          }
+        });
+      }
   }
 });
 // login
